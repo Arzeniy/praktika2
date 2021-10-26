848beeff69fe9a1da5c50ef9bd0dfc800184b665
Arzeniy <arsenijtsurka@gmail.com>
2021-10-26 15:16:09 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+new Vue({
+    el: '#app',
+    data: {
+        students: [],
+        search: '',
+        piece: '',
+        addForm: {
+            mark: '',
+            group: '',
+            isDonePr: false,
+            name: '',
+        },
+        converter: {
+            from: '',
+            to: '',
+            amount: '',
+        },
+        ccy: [],
+    },
+    mounted: function () {
+        axios.get('http://46.101.212.195:3000/students').then((response) => this.students = response.data)
+        axios.get('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5').then((response) => this.ccy = response.data)
+    },
+    methods: {
+        deleteStudent(studId) {
+            this.students = this.students.filter(elem => {
+                return elem._id != studId;
+            });
+        },
+        addStudent() {
+            const id = this.students.length + 1;
+            this.students.push({ id, ...this.addForm });
+        },
+    },
+}); 
